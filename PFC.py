978d57f895ea0ac9d7b8cfc48b22697742c86084
Nathgrsm <ngrossmann@guardiaschool.fr>
2022-11-10 08:52:48 +0000

    Ajout de l'Easter Egg du PFC.

--- PFC.py
+++ PFC.py
@@ -1,9 +1,9 @@
 from random import *
 
 def choix_utilisateur():
-    joueur = input("Choisir entre : Pierre / Papier / Ciseaux :\n")
-    while joueur not in ["Pierre","Papier","Ciseaux"]:
-        joueur = input("Pierre\nPapier\nCiseaux\n")
+    joueur = input("Choisir entre : Pierre / Papier / Ciseaux / Puit:\n")
+    while joueur not in ["Pierre","Papier","Ciseaux","Puit"]:
+        joueur = input("Pierre\nPapier\nCiseaux\nPuit\n")
     return(joueur)
 def choix_ordinateur():
     n = randint(1,3)
@@ -40,3 +40,10 @@ elif a == "Ciseaux" and b == "Pierre":
     print("Perdu")
 elif a == "Ciseaux" and b == "Ciseaux":
     print("Egalite")
+
+if a == "Puit" and b == "Papier":
+    print("Gagné")
+elif a == "Puit" and b == "Pierre":
+    print("Gagné")
+elif a == "Puit" and b == "Ciseaux":
+    print ("Gagné")
